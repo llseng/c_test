55f75a24bdf78fbbf6b8620cebb1e7f695b4db69
llseng <1300904522@qq.com>
2020-06-02 12:10:08 +0800

    setjmp 测试

---
+++ type_test/int_str.c
@@ -0,0 +1,17 @@
+/*
+* @Author: llseng
+* @Date:   2020-05-26 18:16:21
+* @Last Modified by:   llseng
+* @Last Modified time: 2020-06-01 11:35:14
+*/
+
+#include <stdio.h>
+
+int main( int argc, char const *argv[] ) {
+    double a = 0.1,b = 0.2;
+    double c = a + b;
+
+    printf("time %f, %f, %f\n", a, b, c);
+
+    return 0;
+}
