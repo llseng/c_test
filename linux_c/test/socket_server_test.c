828fe5d5ad1991ea6c6e6ae152de596c5abe5b12
llseng <1300904522@qq.com>
2020-06-04 16:39:31 +0800

    linux socket test3

--- linux_c/test/socket_server_test.c
+++ linux_c/test/socket_server_test.c
@@ -2,9 +2,10 @@
 * @Author: llseng
 * @Date:   2020-06-03 16:05:10
 * @Last Modified by:   llseng
-* @Last Modified time: 2020-06-04 14:59:28
+* @Last Modified time: 2020-06-04 16:29:22
 */
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,6 +24,18 @@
 // 监听地址
 #define LISTEN_ADDR "127.0.0.1"
 
+extern int errno;
+
+void sock_close( int sockfd ) {
+    int status;
+    printf( "close socket %d\n", sockfd );
+    // 关闭文件;关闭socket连接
+    status = close( sockfd );
+    if( status < 0 ) {
+        perror( "close" );
+    }
+}
+
 int main( int argc, char const *argv[] ) {
 
     struct sockaddr_in listen_addr, client_addr;
@@ -30,7 +43,7 @@ int main( int argc, char const *argv[] ) {
     int socket_domain, socket_type;
     int bind_status, listen_status, close_status;
     int client_addr_len, w_count, recv_len, send_len;
-    int recv_buf[MAX_LINE];
+    char recv_buf[MAX_LINE], * find;
 
     socket_domain = AF_INET;
     socket_type = SOCK_STREAM;
@@ -46,7 +59,7 @@ int main( int argc, char const *argv[] ) {
     listen_addr.sin_port = htons( LISTEN_PORT );
     listen_addr.sin_addr.s_addr = inet_addr( LISTEN_ADDR );
     // 对shocket定位;绑定地址与端口
-    bind_status = bind( socket_fd, &listen_addr, sizeof( listen_addr ) );
+    bind_status = bind( socket_fd, (struct sockaddr *)&listen_addr, sizeof( listen_addr ) );
     if( bind_status < 0 ) {
         perror( "bind" );
         exit( 1 );
@@ -79,10 +92,28 @@ int main( int argc, char const *argv[] ) {
         recv_len = recv( client_fd, recv_buf, MAX_LINE, 0 );
         if( recv_len <= 0 ) {
             perror( "recv" );
+            sock_close( client_fd );
             continue;
             // exit( 1 );
         }
-        printf( "recv_len %d, recv_buf_len %d, recv_buf %s\n", recv_len, strlen( recv_buf ), recv_buf );
+        printf( "recv_len %d, recv_buf_len %d \nrecv_buf|%s|\n", recv_len, strlen( recv_buf ), recv_buf );
+        // 获取后续数据
+        while( ( find = strchr( recv_buf, '\n' ) ) == NULL ) {
+
+            bzero( recv_buf, sizeof( recv_buf ) );
+            // 经socket接收数据
+            recv_len = recv( client_fd, recv_buf, MAX_LINE, 0 );
+            if( recv_len <= 0 ) {
+                perror( "add recv" );
+                sock_close( client_fd );
+                break;
+                // exit( 1 );
+            }
+            printf( "recv_len %d, recv_buf_len %d \nrecv_buf_add|%s|\n", recv_len, strlen( recv_buf ), recv_buf );
+        }
+        if( errno > 0 ) {
+            continue;
+        }
 
         // 经socket传送数据
         // send_len = send( client_fd, recv_buf, sizeof( recv_buf ), 0 );
@@ -92,25 +123,15 @@ int main( int argc, char const *argv[] ) {
             continue;
             // exit( 1 );
         }
-        printf( "send_len %d, send_buf_len %d, send_buf %s\n", send_len, strlen( recv_buf ), recv_buf );
+        printf( "send_len %d, send_buf_len %d \nsend_buf|%s|\n", send_len, strlen( recv_buf ), recv_buf );
 
-        printf( "close client %d\n", client_fd );
-        // 关闭文件;关闭客户端连接
-        close_status = close( client_fd );
-        if( close_status < 0 ) {
-            perror( "close" );
+        sock_close( client_fd );
+        if( errno > 0 ) {
             continue;
-            // exit( 1 );
         }
     }
 
-    printf( "close server %d\n", socket_fd );
-    // 关闭文件;关闭客户端连接
-    close_status = close( socket_fd );
-    if( close_status < 0 ) {
-        perror( "close" );
-        exit( 1 );
-    }
+    sock_close( socket_fd );
 
     return 0;
 }
