d1459feb889a60cbf815501ecc25ab626f3c65dd
llseng <1300904522@qq.com>
2020-06-24 18:07:49 +0800

    int 数组

--- type_test/int.c
+++ type_test/int.c
@@ -2,7 +2,7 @@
 * @Author: llseng
 * @Date:   2020-06-02 12:28:31
 * @Last Modified by:   llseng
-* @Last Modified time: 2020-06-02 12:31:58
+* @Last Modified time: 2020-06-24 17:03:26
 */
 
 #include <stdio.h>
@@ -17,6 +17,17 @@ int main( void ) {
         b--;
 
         printf("a = %d, b = %d\n", a, b);
+    }
+
+    int num_l[] = {0,1,2,3,4,5,6,7,8,9};
+
+    printf("*num_l = %d\n", *num_l = 5);
+    *num_l = 0;
+    printf("*num_l = %d\n", *num_l);
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("num_l[%d] = %d\n", i, num_l[i]);
     }
 
     return 0;
--- type_test/pter.c
+++ type_test/pter.c
@@ -2,6 +2,15 @@
 * @Author: llseng
 * @Date:   2020-06-18 11:58:56
 * @Last Modified by:   llseng
-* @Last Modified time: 2020-06-18 11:59:05
+* @Last Modified time: 2020-06-24 18:05:56
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
