a3fec0b14bcb7ca559debb8ccb55a449748f805b
llseng <1300904522@qq.com>
2020-06-30 14:54:50 +0800

    mq_attr 默认数值

--- linux_c/test/test.c
+++ linux_c/test/test.c
@@ -2,16 +2,32 @@
 * @Author: llseng
 * @Date:   2020-06-28 11:27:04
 * @Last Modified by:   llseng
-* @Last Modified time: 2020-06-28 11:29:49
+* @Last Modified time: 2020-06-30 14:52:33
 */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <mqueue.h>
 
 int main(int argc, char const *argv[])
 {
     printf("WNOHANG : %d\n", WNOHANG);
+
+    mqd_t mq;
+    struct mq_attr mqAttr;
+    mq = mq_open( "mq_test", O_CREAT|O_EXCL );
+
+    mq_getattr( mq, &mqAttr );
+
+    printf("mq_flags %ld\n", mqAttr.mq_flags); /* Message queue flags. 0 or O_NONBLOCK */
+
+    printf("mq_maxmsg %ld\n", mqAttr.mq_maxmsg); /* Maximum number of messages.  */
+
+    printf("mq_msgsize %ld\n", mqAttr.mq_msgsize); /* Maximum message size.  */
+
+    printf("mq_curmsgs %ld\n", mqAttr.mq_curmsgs); /* Number of messages currently queued.  */
+
     return 0;
 }
