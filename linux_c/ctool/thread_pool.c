850841a69a285fe850a9b25a75d5e42c12f7db5f
llseng <1300904522@qq.com>
2020-07-07 16:19:19 +0800

    线程池管理与工作函数

--- linux_c/ctool/thread_pool.c
+++ linux_c/ctool/thread_pool.c
@@ -2,8 +2,9 @@
  * @Author: llseng 
  * @Date: 2020-07-06 16:01:01 
  * @Last Modified by: llseng
- * @Last Modified time: 2020-07-06 17:08:01
+ * @Last Modified time: 2020-07-07 16:18:14
  */
+#include <stdlib.h>
 #include <sys/time.h>
 #include "thread_pool.h"
 
@@ -18,18 +19,105 @@ wm_thread_pool_task_t *wm_thread_pool_malloc_task( void *(*func)(void *), void *
     return task;
 }
 
-void *wm_thread_pool_manager_run( wm_thread_pool_t *pool ) {
+void *wm_thread_pool_manager_run( void *arg ) {
+    wm_thread_pool_t *pool = (wm_thread_pool_t *)arg;
+    
+    struct timespec req;
+    req.tv_sec = 0;
+    req.tv_nsec = 100000000;
 
+    while ( 1 )
+    {
+        if( wm_thread_pool_lock( pool ) != 0 ) break;
+
+        int i, lave_idle_count, lave_thread_count, wait_create_count;
+        lave_idle_count = pool->max_idle_count - pool->idle_thread_count;
+        lave_thread_count = pool->max_thread_count - pool->thread_count;
+        wait_create_count = lave_thread_count >= lave_idle_count? lave_idle_count: lave_thread_count;
+
+        for( i = 0; i < wait_create_count; i++ ) {
+            pthread_t worker_tid;
+            pthread_attr_t worker_attr;
+            /*创建工作线程*/
+            if( pthread_attr_init( &worker_attr ) != 0 ) break;
+            if( pthread_attr_setdetachstate( &worker_attr, PTHREAD_CREATE_DETACHED ) != 0 ) break;
+            if( pthread_create( &worker_tid, &worker_attr, wm_thread_pool_worker_run, arg ) != 0 ) break;
+
+            pool->thread_count++;
+        }
+
+        if( wm_thread_pool_unlock( pool ) != 0 ) break;
+    
+        if( nanosleep( &req, NULL ) != 0 ) break;
+    }
+
+    pthread_exit( NULL );
 }
 
-void *wm_thread_pool_worker_run( wm_thread_pool_t *pool ) {
+void *wm_thread_pool_worker_run( void *arg ) {
+    wm_thread_pool_t *pool = (wm_thread_pool_t *)arg;
 
+    int result;
+    wm_thread_pool_task_t *task;
+
+    while( 1 ) {
+        if( wm_thread_pool_lock( pool ) != 0 ) break;
+
+        pool->idle_thread_count++;
+
+        result = wm_thread_pool_timedwait( pool, pool->idle_msec );
+        /*线程池关闭*/
+        if( pool->shutdown ) {
+            pool->thread_count--;
+            pool->idle_thread_count--;
+            if( wm_thread_pool_unlock( pool ) != 0 ) break;
+            break;
+        }
+
+        if( result == 0 ) {
+            /*任务通知信号*/
+            if( pool->task_head == pool->task_tail ) {
+                task = NULL;
+            }else{
+                task = pool->task_head->next;
+                pool->task_head->next = task->next;
+                pool->task_count--;
+            }
+
+            if( pool->task_head->next == NULL ) {
+                pool->task_tail = pool->task_head;
+                pool->task_count = 0;
+            }
+        }else if( result == ETIMEDOUT ) {
+            /*等待通知信号超时*/
+            if( pool->idle_thread_count > pool->max_idle_count ) {
+                pool->thread_count--;
+                pool->idle_thread_count--;
+                if( wm_thread_pool_unlock( pool ) != 0 ) break;
+                break;
+            }
+        }else{
+            if( wm_thread_pool_unlock( pool ) != 0 ) break;
+            break;
+        }
+
+        pool->idle_thread_count--;
+        if( wm_thread_pool_unlock( pool ) != 0 ) break;
+
+        if( task != NULL ) task->func( task->param ); //执行任务
+
+        free( task );
+
+    }
+
+    pthread_exit( NULL );
 }
 
 int wm_thread_pool_init( wm_thread_pool_t *pool ) {
     wm_thread_pool_task_t *start_task = wm_thread_pool_malloc_task(NULL, NULL, NULL);
     if( start_task == NULL ) return 1;
 
+    if( pthread_attr_init( &pool->manager_attr ) != 0 ) return -3;
     if( pthread_cond_init( &pool->cond, NULL ) != 0 ) return -1;
     if( pthread_mutex_init( &pool->mutex, NULL ) != 0 ) return -2;
 
@@ -38,7 +126,7 @@ int wm_thread_pool_init( wm_thread_pool_t *pool ) {
 
     pool->task_count = 0;
     pool->thread_count = 0;
-    pool->busy_thread_count = 0;
+    // pool->busy_thread_count = 0;
     pool->idle_thread_count = 0;
 
     pool->idle_msec = 1000000;
@@ -46,6 +134,10 @@ int wm_thread_pool_init( wm_thread_pool_t *pool ) {
     pool->max_idle_count = 4;
 
     pool->shutdown = 0;
+
+    /*创建管理线程*/
+    if( pthread_attr_setdetachstate( &pool->manager_attr, PTHREAD_CREATE_DETACHED ) != 0 ) return -4;
+    if( pthread_create( &pool->manager_tid, &pool->manager_attr, wm_thread_pool_manager_run, (void *)pool ) != 0 ) return -5;
 
     return 0;
 }
@@ -110,6 +202,7 @@ int wm_thread_pool_set_idle_msec( wm_thread_pool_t *pool, unsigned int msec ) {
     return 0;
 }
 
+/*
 int wm_thread_pool_lock( wm_thread_pool_t *pool ) {
     return pthread_mutex_lock( &pool->mutex );
 }
@@ -129,6 +222,7 @@ int wm_thread_pool_broadcast( wm_thread_pool_t *pool ) {
 int wm_thread_pool_wait( wm_thread_pool_t *pool ) {
     return pthread_cond_wait( &pool->cond, &pool->mutex );
 }
+*/
 
 int wm_thread_pool_timedwait( wm_thread_pool_t *pool, unsigned int msec ) {
     struct timeval tv;
