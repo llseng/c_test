cac4a74a3b2a91fadcac02093c84af1e40573678
llseng <1300904522@qq.com>
2020-07-08 12:31:06 +0800

    线程池关闭测试

--- linux_c/ctool/test.c
+++ linux_c/ctool/test.c
@@ -5,6 +5,7 @@
  * @Last Modified time: 2020-07-07 16:45:03
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "thread_pool.h"
 // #include "thread_pool.c"
@@ -21,8 +22,9 @@ static void check_result( char *str, int result ) {
 
 static void * test_func(void *arg) {
     printf( "test_func arg = %d start\n", *(int *)arg );
-    sleep( 10 );
+    sleep( *(int *)arg );
     printf( "test_func arg = %d end\n", *(int *)arg );
+    free( arg );
 }
 
 int main(int argc, char const *argv[])
@@ -40,22 +42,30 @@ int main(int argc, char const *argv[])
     check_result("wm_thread_pool_set_idle_msec", res);
     if( res != 0 ) return 0;
 
-    for ( i = 0; i < 10; i++)
+    for ( i = 0; i < 100; i++)
     {
-        res = wm_thread_pool_add_task( &pool, test_func, (void *)&i );
+        int *i_p = (int *)malloc( sizeof( int ) );
+        *i_p = i;
+        res = wm_thread_pool_add_task( &pool, test_func, (void *)i_p );
         check_result("wm_thread_pool_add_task", res);
         if( res != 0 ) return 0;
     }
-    
-    while( 1 ) {
+    i = 1;
+    while( i-- ) {
         sleep( 1 );
-        printf( "task = %d thread = %d idle_thread = %d max_idle_count = %d\n", pool.task_count, pool.thread_count, pool.idle_thread_count, pool.max_idle_count );
+        printf( "task = %u thread = %u idle_thread = %u\n", pool.task_count, pool.thread_count, pool.idle_thread_count );
         fflush( stdout );
     }
 
-    // res = wm_thread_pool_shutdown( &pool );
-    // check_result("wm_thread_pool_shutdown", res);
-    // if( res != 0 ) return 0;
+    res = wm_thread_pool_shutdown( &pool );
+    check_result("wm_thread_pool_shutdown", res);
+    if( res != 0 ) return 0;
+    
+    while( 1 ) {
+        sleep( 1 );
+        printf( "task = %u thread = %u idle_thread = %u\n", pool.task_count, pool.thread_count, pool.idle_thread_count );
+        fflush( stdout );
+    }
 
     return 0;
 }
