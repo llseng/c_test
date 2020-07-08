80d3c2a4358c6a18b82b48b7b84fafc7fce98706
llseng <1300904522@qq.com>
2020-07-08 17:03:55 +0800

    变参宏 __VA_ARGS__ 测试

--- type_test/preprocessor.c
+++ type_test/preprocessor.c
@@ -2,7 +2,7 @@
  * @Author: llseng 
  * @Date: 2020-07-07 10:06:59 
  * @Last Modified by: llseng
- * @Last Modified time: 2020-07-07 10:24:59
+ * @Last Modified time: 2020-07-08 16:22:52
  */
 #include <stdio.h>
 
@@ -19,6 +19,8 @@ static int return_int( int i ) {
 #define return_int_5( i ) ( i )
 #define return_int_6( i ) printf( "printf i = %d\n", (i) )
 
+#define return_sum( fmt, ... ) printf( fmt, ##__VA_ARGS__ )
+
 int main(int argc, char const *argv[])
 {
     int i = 1;
@@ -31,5 +33,8 @@ int main(int argc, char const *argv[])
     printf( "return_int_5( %d ) = %d\n", i, return_int_5( i ) );
     i++;
     printf( "return_int_6( %d ) = %d\n", i, return_int_6( i ) );
+
+    return_sum( "printf %d %d %d\n",1, 2, 3 );
+    return_sum( "printf %%d %%d %%d\n" );
     return 0;
 }
