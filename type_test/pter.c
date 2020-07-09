82a70d829dafcb0444dc3ac422d01c0398b84db7
llseng <1300904522@qq.com>
2020-07-09 17:41:33 +0800

    二级指针测试

--- type_test/pter.c
+++ type_test/pter.c
@@ -2,7 +2,7 @@
 * @Author: llseng
 * @Date:   2020-06-18 11:58:56
  * @Last Modified by: llseng
- * @Last Modified time: 2020-07-08 19:22:26
+ * @Last Modified time: 2020-07-09 17:36:00
 */
 
 #include <stdio.h>
@@ -18,73 +18,54 @@ struct book_attr {
 };
 typedef struct book_attr book_attr_t;
 
-static book_attr_t bal[ 10 ], ba, *bal_p, *ba_p;
-
-static void voidp_to_intp( void *p ) {
-    // printf("p->author = %s\n", ((book_attr_t *)p)->author);
-    printf("p = %d\n", *(int *)p );
-}
-
-static void *test_void( char* param ){
-    printf( "param = %s\n", param );
-}
-
 int main(int argc, char const *argv[])
 {
+    int i;
+    book_attr_t a, b, c, **p;
 
-    bal_p = bal;
-    ba_p = &ba;
+    a.line = 1;
+    strcpy( a.title, "a" );
+    strcpy( a.author, "a" );
+    strcpy( a.intro, "a" );
 
-    ba.line = 10;
-    strcpy( ba.title, "test");
-    strcpy( ba.author, "llseng");
-    strcpy( ba.intro, "test,test,test");
+    b.line = 2;
+    strcpy( b.title, "b" );
+    strcpy( b.author, "b" );
+    strcpy( b.intro, "b" );
 
-    printf("bal sizeof = %u\n", sizeof( bal ) ); //bal sizeof = 1440
-    printf("ba sizeof = %u\n", sizeof( ba ) ); //ba sizeof = 144
-    printf("bal_p sizeof = %u\n", sizeof( bal_p ) ); //bal_p sizeof = 8
-    printf("ba_p sizeof = %u\n", sizeof( ba_p ) ); //ba_p sizeof = 8
-    printf("*bal_p sizeof = %u\n", sizeof( *bal_p ) ); //*bal_p sizeof = 144
-    printf("*ba_p sizeof = %u\n", sizeof( *ba_p ) ); //*ba_p sizeof = 144
+    c.line = 3;
+    strcpy( c.title, "c" );
+    strcpy( c.author, "c" );
+    strcpy( c.intro, "c" );
 
-    int num = 123;
+    p = (book_attr_t **)calloc( 1, sizeof( book_attr_t * ) );
 
-    // voidp_to_intp( (void *)&ba );
-    voidp_to_intp( (void *)&num );
+    printf( "sizeof( p ) = %d\n", sizeof( p ) );
+    printf( "sizeof( *p ) = %d\n", sizeof( *p ) );
+    printf( "sizeof( **p ) = %d\n", sizeof( **p ) );
 
-    void * ba_void_p;
-    book_attr_t ba_void = {100, "test", "whymust", "test void *"};
+    printf( "p addr %p\n", &p );
 
-    ba_void_p = &ba_void;
+    *(p+0) = &a;
+    p = (book_attr_t **)realloc( p, 3 * sizeof( book_attr_t *) );
+    *(p+1) = &b;
+    *(p+2) = &c;
 
-    void *(*f_1)(void *);
-    f_1 = test_void;
-
-    f_1( (void *)((book_attr_t *)ba_void_p)->intro );
+    printf( "a addr %p\n", &a );
+    printf( "b addr %p\n", &b );
+    printf( "c addr %p\n", &c );
+    printf( "p addr %p\n", &p );
     
-    int ba_arr_len = 2;
-    book_attr_t *ba_arr = (book_attr_t *)calloc( ba_arr_len, sizeof( book_attr_t ) );
+    for ( i = 0; i < 3; i++ )
+    {
+        printf( "addr %p\n", *(p +i) );
 
-    strcpy( ba_arr[0].title, "0" );
-    strcpy( ba_arr[1].title, "1" );
-
-    printf( "ba_arr->title %s\n", ba_arr[0].title );
-    printf( "ba_arr->title %s\n", ba_arr[1].title );
-
-    ba_arr[0] = ba;
-
-
-    printf( "ba_arr->title %s\n", ba_arr[0].title );
-    printf( "ba_arr->author %s\n", ba_arr[0].author );
-    printf( "ba_arr->intro %s\n", ba_arr[0].intro );
-    printf( "ba_arr->title %s\n", ba_arr[1].title );
+        printf( "p.line %d\n", (*(p +i))->line );
+        printf( "p.title '%s'\n", (*(p +i))->title );
+        printf( "p.author '%s'\n", (*(p +i))->author );
+        printf( "p.intro '%s'\n", (*(p +i))->intro );
+    }
     
-    strcpy( ba.title, "test0");
-
-    printf( "ba_arr->title %s\n", ba_arr[0].title );
-    printf( "ba_arr->author %s\n", ba_arr[0].author );
-    printf( "ba_arr->intro %s\n", ba_arr[0].intro );
-    printf( "ba_arr->title %s\n", ba_arr[1].title );
 
     return 0;
 }
