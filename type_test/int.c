fd920cd611a7b8aa3d5fdfcd44b87afa028e0f99
llseng <1300904522@qq.com>
2020-07-09 19:32:17 +0800

    test

--- type_test/int.c
+++ type_test/int.c
@@ -1,8 +1,8 @@
 /*
 * @Author: llseng
 * @Date:   2020-06-02 12:28:31
-* @Last Modified by:   llseng
-* @Last Modified time: 2020-06-24 17:03:26
+ * @Last Modified by: llseng
+ * @Last Modified time: 2020-07-09 18:21:45
 */
 
 #include <stdio.h>
@@ -10,25 +10,11 @@
 int main( void ) {
     int a = 5, i;
     unsigned int b = 5;
+    
+    printf( "a-- %d\n", a-- );
+    printf( "--b %d\n", --b );
 
-    for (i = 0; i < 6; i++)
-    {
-        a--;
-        b--;
-
-        printf("a = %d, b = %d\n", a, b);
-    }
-
-    int num_l[] = {0,1,2,3,4,5,6,7,8,9};
-
-    printf("*num_l = %d\n", *num_l = 5);
-    *num_l = 0;
-    printf("*num_l = %d\n", *num_l);
-
-    for (i = 0; i < 10; i++)
-    {
-        printf("num_l[%d] = %d\n", i, num_l[i]);
-    }
-
+    printf( "a %d\n", a );
+    printf( "b %d\n", b );
     return 0;
 }
