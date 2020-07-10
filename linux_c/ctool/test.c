d8c6232b052cf60b22061ee4ff804e30e18e2817
llseng <1300904522@qq.com>
2020-07-10 18:06:36 +0800

    日志测试

--- linux_c/ctool/test.c
+++ linux_c/ctool/test.c
@@ -2,13 +2,20 @@
  * @Author: llseng 
  * @Date: 2020-07-07 16:44:16 
  * @Last Modified by: llseng
- * @Last Modified time: 2020-07-08 14:58:54
+ * @Last Modified time: 2020-07-10 18:03:33
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "thread_pool.h"
-// #include "thread_pool.c"
+#include "logger.h"
+
+#define DEBUG 1
+
+#if DEBUG
+#include "thread_pool.c"
+#include "logger.c"
+#endif
 
 static void check_result( char *str, int result ) {
     if( result == 0 ) {
@@ -69,10 +76,22 @@ int main(int argc, char const *argv[])
     res = wm_thread_pool_shutdown( &pool );
     check_result("wm_thread_pool_shutdown", res);
     if( res != 0 ) return 0;
-    
+
+    wm_logger_t logger;
+    wm_logger_handler_t log_handler;
+
+    wm_logger_init( &logger, "logger_test" );
+    wm_logger_handler_init( &log_handler, "../log_test.log", LOG_DEBUG );
+    wm_logger_add_handler( &logger, &log_handler );
+
     while( 1 ) {
         sleep( 1 );
         printf( "task = %u thread = %u idle_thread = %u\n", pool.task_count, pool.thread_count, pool.idle_thread_count );
+
+        wm_log_info( &logger, "task = %u thread = %u idle_thread = %u\n", pool.task_count, pool.thread_count, pool.idle_thread_count );
+
+        fputs( "123456789\n", log_handler.fd );
+
         fflush( stdout );
     }
 
