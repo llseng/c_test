42ac4393425cc71d550e2b169096d30108cf6a76
llseng <1300904522@qq.com>
2020-07-10 18:06:23 +0800

    logger日志完成

--- linux_c/ctool/logger.c
+++ linux_c/ctool/logger.c
@@ -2,10 +2,13 @@
  * @Author: llseng 
  * @Date: 2020-07-09 16:12:04 
  * @Last Modified by: llseng
- * @Last Modified time: 2020-07-09 19:32:04
+ * @Last Modified time: 2020-07-10 17:46:15
  */
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <time.h>
 #include "logger.h"
 
 int wm_logger_init( wm_logger_t *logger, char *name ) {
@@ -58,33 +61,138 @@ int wm_logger_pop_handler( wm_logger_t *logger ) {
     return 0;
 }
 
-int wm_logger_write( wm_logger_t *logger, int level, ... );
+int wm_logger_write( wm_logger_t *logger, int level, char *fmt, ... ) {
+    if( !logger->handler_len ) return 0;
+    int i;
+    char logger_str[ strlen( logger->name ) + 80 + 4 + strlen( fmt ) ], time_str[ 80 ], time_format[] = "%Y-%m-%d %H:%M:%S", level_name[40];
+    time_t now_time = time( NULL );
+    struct tm * time_tm = localtime( &now_time );
+    
+    wm_get_level_name( level, level_name );
+    strftime( time_str, 80, time_format, time_tm );
+    sprintf( logger_str, "[%s] %s.%s: %s", time_str, logger->name, level_name, fmt );
+    // strcat( logger_str, fmt );
+
+    va_list args;
+    va_start( args, fmt );
+
+    for( i = 0; i < logger->handler_len; i++ ) {
+        char mag_str[ (*( logger->handler_arr + i ))->max_line_size ];
+        vsprintf( mag_str, logger_str, args );
+        wm_logger_handler_write( *( logger->handler_arr + i ), level, mag_str );
+    }
+    
+    va_end( args );
+
+    return 0;
+}
+
+int wm_get_level_name( unsigned int level, char *str ) {
+    switch (level)
+    {
+    case LOG_DEBUG:
+        strcpy( str ,"DEBUG" );
+        break;
+    case LOG_INFO:
+        strcpy( str ,"INFO" );
+        break;
+    case LOG_NOTICE:
+        strcpy( str ,"NOTICE" );
+        break;
+    case LOG_WARNING:
+        strcpy( str ,"WARNING" );
+        break;
+    case LOG_ERROR:
+        strcpy( str ,"ERROR" );
+        break;
+    case LOG_CRITICAL:
+        strcpy( str ,"CRITICAL" );
+        break;
+    
+    default:
+        strcpy( str ,"DEBUG" );
+        break;
+    }
+
+    return 0;
+}
 
 int wm_logger_handler_init( wm_logger_handler_t *handler, char *file_addr, unsigned int level ) {
-    char *pos;
-    pos = strrchr( file_addr, '/' );
-    pos = pos? pos + 1: file_addr;
-
-    handler->file_dir = (char *)calloc( abs( pos - file_addr ) + 1, sizeof( char ) );
+    handler->file_dir = (char *)calloc( 1, sizeof( char ) );
     if( handler->file_dir == NULL ) return -1;
-    handler->file_name = (char *)calloc( strlen( pos ) + 1, sizeof( char ) );
+    handler->file_name = (char *)calloc( 1, sizeof( char ) );
     if( handler->file_name == NULL ) return -2;
 
-    strcpy( handler->file_name, pos );
+    if( wm_logger_handler_set_file_addr( handler, file_addr ) != 0 ) return 1;
+    if( wm_logger_handler_set_level( handler, level ) != 0 ) return 2;
+    if( wm_logger_handler_set_max_line_size( handler, DEFAULT_MAX_LINE_SIZE ) != 0 ) return 3;
+    if( wm_logger_handler_set_max_file_size( handler, DEFAULT_MAX_FILE_SIZE ) != 0 ) return 4;
 
-    handler->level = level == NULL? LOG_DEBUG: level;
+    return 0;
+}
+
+int wm_logger_handler_destroy( wm_logger_handler_t *handler ) {
+    if( handler->fd != NULL ) fclose( handler->fd );
+    if( handler->file_dir != NULL ) free( handler->fd );
+    if( handler->file_name != NULL ) free( handler->fd );
+
+    handler->level = LOG_DEBUG;
     handler->max_line_size = DEFAULT_MAX_LINE_SIZE;
     handler->max_file_size = DEFAULT_MAX_FILE_SIZE;
 
     return 0;
 }
 
-int wm_logger_handler_destroy( wm_logger_handler_t *handler );
+int wm_logger_handler_set_file_addr( wm_logger_handler_t *handler, char *file_addr ){
+    
+    char *pos;
+    int dir_len, name_len;
+    pos = strrchr( file_addr, '/' );
+    pos = pos? pos: file_addr;
 
-int wm_logger_handler_set_file_addr( wm_logger_handler_t *handler, char *file_addr );
+    dir_len = pos != file_addr? abs( pos - file_addr ): 0;
+    name_len = strlen( pos + 1 );
 
-int wm_logger_handler_set_level( wm_logger_handler_t *handler, unsigned int level );
+    handler->file_dir = (char *)realloc( handler->file_dir, (dir_len + 1) * sizeof( char ) );
+    if( handler->file_dir == NULL ) return -1;
+    handler->file_name = (char *)realloc( handler->file_name, (name_len + 1) * sizeof( char ) );
+    if( handler->file_name == NULL ) return -2;
 
-int wm_logger_handler_set_line_size_max( wm_logger_handler_t *handler, unsigned int line_size_max );
+    if( strcpy( handler->file_name, pos + 1 ) == NULL ) return -3;
+    if( strncpy( handler->file_dir, file_addr, dir_len ) == NULL ) return -4;
 
-int wm_logger_handler_set_file_size_max( wm_logger_handler_t *handler, unsigned int file_size_max );
+    handler->fd = fopen( file_addr, "ab+" );
+    if( handler->fd == NULL ) return -5;
+
+    return 0;
+}
+
+int wm_logger_handler_set_level( wm_logger_handler_t *handler, unsigned int level ) {
+    handler->level = level? level: LOG_DEBUG;
+
+    return 0;
+}
+
+int wm_logger_handler_set_max_line_size( wm_logger_handler_t *handler, unsigned int max_line_size ) {
+    handler->max_line_size = max_line_size? max_line_size: DEFAULT_MAX_LINE_SIZE;
+
+    return 0;
+}
+
+int wm_logger_handler_set_max_file_size( wm_logger_handler_t *handler, unsigned int max_file_size ) {
+    handler->max_file_size = max_file_size? max_file_size: DEFAULT_MAX_FILE_SIZE;
+
+    return 0;
+}
+
+int wm_logger_handler_write( wm_logger_handler_t *handler, unsigned int level, char *message ) {
+
+    if( handler->fd == NULL ) return 1;
+    if( strlen( message ) == 0 ) return 2;
+
+    int put_len;
+    put_len = fputs( message, handler->fd );
+    if( put_len < 0 ) return -1; 
+
+    return 0;
+}
--- linux_c/ctool/logger.h
+++ linux_c/ctool/logger.h
@@ -2,7 +2,7 @@
  * @Author: llseng 
  * @Date: 2020-07-08 15:47:35 
  * @Last Modified by: llseng
- * @Last Modified time: 2020-07-09 18:41:39
+ * @Last Modified time: 2020-07-10 16:11:42
  */
 #ifndef _WM_LOGGER_H_
 #define _WM_LOGGER_H_
@@ -21,12 +21,6 @@
 #define DEFAULT_MAX_LINE_SIZE 8192
 #define DEFAULT_MAX_FILE_SIZE 6553600
 
-typedef struct wm_logger {
-    char *name;
-    unsigned int handler_len;
-    wm_logger_handler_t **handler_arr;
-} wm_logger_t;
-
 typedef struct wm_logger_handler {
     FILE *fd;
     char *file_dir;
@@ -35,6 +29,12 @@ typedef struct wm_logger_handler {
     unsigned int max_line_size;
     unsigned int max_file_size;
 } wm_logger_handler_t;
+
+typedef struct wm_logger {
+    char *name;
+    unsigned int handler_len;
+    wm_logger_handler_t **handler_arr;
+} wm_logger_t;
 
 int wm_logger_init( wm_logger_t *logger, char *name );
 
@@ -46,7 +46,7 @@ int wm_logger_add_handler( wm_logger_t *logger, wm_logger_handler_t *handler );
 
 int wm_logger_pop_handler( wm_logger_t *logger );
 
-int wm_logger_write( wm_logger_t *logger, int level, ... );
+int wm_logger_write( wm_logger_t *logger, int level, char *fmt, ... );
 
 int wm_logger_handler_init( wm_logger_handler_t *handler, char *file_addr, unsigned int level );
 
@@ -56,16 +56,20 @@ int wm_logger_handler_set_file_addr( wm_logger_handler_t *handler, char *file_ad
 
 int wm_logger_handler_set_level( wm_logger_handler_t *handler, unsigned int level );
 
-int wm_logger_handler_set_line_size_max( wm_logger_handler_t *handler, unsigned int line_size_max );
+int wm_logger_handler_set_max_line_size( wm_logger_handler_t *handler, unsigned int max_line_size );
 
-int wm_logger_handler_set_file_size_max( wm_logger_handler_t *handler, unsigned int file_size_max );
+int wm_logger_handler_set_max_file_size( wm_logger_handler_t *handler, unsigned int max_file_size );
 
-#define wm_log_debug( logger, ... ) wm_logger_write( logger, LOG_DEBUG, ... )
-#define wm_log_info( logger, ... ) wm_logger_write( logger, LOG_INFO, ... )
-#define wm_log_notice( logger, ... ) wm_logger_write( logger, LOG_NOTICE, ... )
-#define wm_log_warning( logger, ... ) wm_logger_write( logger, LOG_WARNING, ... )
-#define wm_log_warn( logger, ... ) wm_logger_write( logger, LOG_WARNING, ... )
-#define wm_log_error( logger, ... ) wm_logger_write( logger, LOG_ERROR, ... )
-#define wm_log_critical( logger, ... ) wm_logger_write( logger, LOG_CRITICAL, ... )
+int wm_logger_handler_write( wm_logger_handler_t *handler, unsigned int level, char *message );
+
+int wm_get_level_name( unsigned int level, char *str );
+
+#define wm_log_debug( logger, fmt, ... ) wm_logger_write( logger, LOG_DEBUG, fmt, ##__VA_ARGS__ )
+#define wm_log_info( logger, fmt, ... ) wm_logger_write( logger, LOG_INFO, fmt, ##__VA_ARGS__ )
+#define wm_log_notice( logger, fmt, ... ) wm_logger_write( logger, LOG_NOTICE, fmt, ##__VA_ARGS__ )
+#define wm_log_warning( logger, fmt, ... ) wm_logger_write( logger, LOG_WARNING, fmt, ##__VA_ARGS__ )
+#define wm_log_warn( logger, fmt, ... ) wm_logger_write( logger, LOG_WARNING, fmt, ##__VA_ARGS__ )
+#define wm_log_error( logger, fmt, ... ) wm_logger_write( logger, LOG_ERROR, fmt, ##__VA_ARGS__ )
+#define wm_log_critical( logger, fmt, ... ) wm_logger_write( logger, LOG_CRITICAL, fmt, ##__VA_ARGS__ )
 
 #endif /*_WM_LOGGER_H_*/
