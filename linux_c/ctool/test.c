a3e145577b34e7c01f0fa7a364b024773c7f476e
llseng <1300904522@qq.com>
2020-07-13 16:40:06 +0800

    非多线程日志测试

--- linux_c/ctool/test.c
+++ linux_c/ctool/test.c
@@ -2,7 +2,7 @@
  * @Author: llseng 
  * @Date: 2020-07-07 16:44:16 
  * @Last Modified by: llseng
- * @Last Modified time: 2020-07-10 19:06:25
+ * @Last Modified time: 2020-07-13 16:27:51
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +10,7 @@
 #include "thread_pool.h"
 #include "logger.h"
 
-#define DEBUG 0
+#define DEBUG 1
 
 #if DEBUG
 #include "thread_pool.c"
@@ -55,6 +55,13 @@ int main(int argc, char const *argv[])
     wm_logger_init( &logger, "logger_test" );
     wm_logger_handler_init( &log_handler, "log_test.log", LOG_DEBUG );
     wm_logger_add_handler( &logger, &log_handler );
+    wm_logger_handler_set_max_file_size( &log_handler, 10485760 );
+    i = 1000000;
+    while( i-- ) {
+        wm_log_info( &logger, "'c:\\Users\\Administrator\\.vscode\\extensions\\ms-vscode.cpptools-0.29.0-insiders2\\debugAdapters\\bin\\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-dqtqpesa.c0f' '--stdout=Microsoft-MIEngine-Out-hydghudb.est' '--stderr=Microsoft-MIEngine-Error-zsiqakoi.cvr' '--pid=Microsoft-MIEngine-Pid-jqtno3k4.0vm' '--dbgExe=D:\\Dev-Cpp\\MinGW64\\bin\\gdb.exe' '--interpreter=mi'" );
+    }
+
+    return 0;
 
     wm_thread_pool_t pool;
 
